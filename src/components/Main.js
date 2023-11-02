11793067d5d3fad94bd702b5dea77496f2d93ab6
DLorangg <lorang.damian.epet20@gmail.com>
2023-11-02 10:26:59 -0300

    First commit

---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div className='main'>
+         <input placeholder='Ingrese un problema matemático'/>
+         <button>Resolver</button> 
+    </div>
+  )
+}
+
+export default Main
